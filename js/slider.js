62cc0e2c1ff0c026539538e140a97fc0ecf15305
Denis <waridesu@gmail.com>
2020-06-17 14:43:47 +0300

    js in persent&allScreen

--- js/slider.js
+++ js/slider.js
@@ -15,27 +15,31 @@ nextBtn.addEventListener('click', () => {
     prevBtn.disabled = false;
     slider.style.transition = 'transform 0.4s ease-in-out';
     counter++;
-    slider.style.transform = 'translateX(' + ((-100 / 3) * counter) + '%)';
-
-
-    if (document.documentElement.clientWidth > 530 && (counter >= sliderImgs.length - 3)) {
-        nextBtn.disabled = true;
+    if(document.documentElement.clientWidth >530){
+        slider.style.transform = 'translateX(' + ((-100 / 3) * counter) + '%)';
+        if (counter >= sliderImgs.length - 3) {
+            nextBtn.disabled = true;
+        }
     }
     else {
+        slider.style.transform = 'translateX(' + (-100 * counter) + '%)';
         if (counter >= sliderImgs.length - 1) {
             nextBtn.disabled = true;
         }
     }
-
-
 });
 
 prevBtn.addEventListener('click', () => {
     nextBtn.disabled = false;
     slider.style.transition = 'transform 0.4s ease-in-out';
     counter--;
-    slider.style.transform = 'translateX(' + ((-100 / 3) * counter) + '%)';
     if (counter <= 0) {
         prevBtn.disabled = true;
+    }
+    if(document.documentElement.clientWidth >530){
+        slider.style.transform = 'translateX(' + ((-100 / 3) * counter) + '%)';
+    }
+    else {
+        slider.style.transform = 'translateX(' + (-100 * counter) + '%)';
     }
 });
